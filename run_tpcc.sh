eb43c63de8ee88441d1599ac8478bc1c50ee1e7b
Yanjun Chen <yanjunc6@uci.edu>
2025-12-08 12:09:45 -0800

    update run tpcc script

--- run_tpcc.sh
+++ run_tpcc.sh
@@ -1,2 +1,4 @@
 rm -r ./tpcc_out
-cargo run --release -- ./examples/tpcc.transact ./tpcc_out --timeout 61920 # 8 hours / 20 cores * 43 jobs
+# cargo run --release -- ./examples/tpcc.transact ./tpcc_out --timeout 61920 # (17.2 hours) 8 hours / 20 cores * 43 jobs
+# cargo run --release -- ./examples/tpcc.transact ./tpcc_out --timeout 600 # (10 min)
+cargo run --release -- ./examples/tpcc.transact ./tpcc_out --timeout 10 # (10 sec)
