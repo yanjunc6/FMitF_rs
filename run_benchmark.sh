ece3646af7d959c69cf669a7baf62b49158f0156
Yanjun Chen <yanjunc6@uci.edu>
2026-01-10 06:07:35 +0800

    Refactor transactions and tables in small_examples and add TPCC benchmark
    - Renamed table `ACCOUNTS` to `A` and updated related transaction parameters and logic in `s1.transact`.
    - Adjusted point redemption and bonus application logic to reflect new naming conventions.
    - Introduced a new TPCC schema in `tpcc_small.transact` with partitions, tables, and transactions for warehouse management.
    - Implemented transactions for new orders, delivery, and payment in the TPCC schema.
    - Added a script `run_benchmark.sh` to facilitate running benchmarks for TPCC and other examples.

--- run_benchmark.sh
+++ run_benchmark.sh
@@ -2,3 +2,5 @@
 cargo run --release -- ./examples/tpcc.transact ./tpcc_out/61920 --timeout 61920 # (17.2 hours) 8 hours / 20 cores * 43 jobs
 cargo run --release -- ./examples/tpcc.transact ./tpcc_out/600 --timeout 600 # (10 min)
 cargo run --release -- ./examples/tpcc.transact ./tpcc_out/10 --timeout 10 # (10 sec)
+cargo run --release -- ./examples/social.transact ./social_out
+cargo run --release -- ./examples/rubis.transact ./rubis_out
