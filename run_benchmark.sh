825363a46afc84e897b4b4303081e6b4b35506f3
Yanjun Chen <yanjunc6@uci.edu>
2026-04-17 06:22:46 +0800

    Update benchmark script to use new output directory and add cache clearing options
    - Changed output paths for TPCC, social, and Rubis benchmarks to use ./data_out/ directory.
    - Added --disable-split and --clear-cache options to all benchmark commands for improved performance and consistency.

--- run_benchmark.sh
+++ run_benchmark.sh
@@ -1,6 +1,6 @@
 # rm -r ./tpcc_out
-cargo run --release -- ./examples/tpcc.transact ./tpcc_out/61920 --timeout 61920 # (17.2 hours) 8 hours / 20 cores * 43 jobs
-cargo run --release -- ./examples/tpcc.transact ./tpcc_out/600 --timeout 600 # (10 min)
-cargo run --release -- ./examples/tpcc.transact ./tpcc_out/10 --timeout 10 # (10 sec)
-cargo run --release -- ./examples/social.transact ./social_out
-cargo run --release -- ./examples/rubis.transact ./rubis_out
+cargo run --release -- ./examples/tpcc.transact ./data_out/tpcc_out/61920 --timeout 61920 --disable-split --clear-cache # (17.2 hours) 8 hours / 20 cores * 43 jobs
+cargo run --release -- ./examples/tpcc.transact ./data_out/tpcc_out/600 --timeout 600 --disable-split --clear-cache # (10 min)
+cargo run --release -- ./examples/tpcc.transact ./data_out/tpcc_out/10 --timeout 10 --disable-split --clear-cache # (10 sec)
+cargo run --release -- ./examples/social.transact ./data_out/social_out --disable-split --clear-cache 
+cargo run --release -- ./examples/rubis.transact ./data_out/rubis_out --disable-split --clear-cache 
